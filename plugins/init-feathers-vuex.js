daf9443a1fac15815a70e57232081b17c63b92f4
Robert Schäfer <git@roschaefer.de>
2018-09-20 16:02:16 +0200

    Fix lint

--- plugins/init-feathers-vuex.js
+++ plugins/init-feathers-vuex.js
@@ -1,5 +1,4 @@
 import createApiClient from '../helpers/createApiClient'
-import feathersVuex from 'feathers-vuex'
 
 const requireModule = require.context(
   // The relative path holding the service modules
@@ -12,7 +11,6 @@ const requireModule = require.context(
 
 export default async ({app, store, req, res}) => {
   const feathersClient = createApiClient({app, req, res})
-  const { service } = feathersVuex(feathersClient, { idField: '_id' })
 
   const servicePlugins = requireModule.keys().map(modulePath => requireModule(modulePath).default(feathersClient))
   servicePlugins.forEach((servicePlugin) => {
