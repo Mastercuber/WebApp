5995cc690fb383064166f80a22b4273384584516
Robert Schäfer <git@roschaefer.de>
2018-09-20 16:58:44 +0200

    A

---
+++ components/mixins/blacklistable.js
@@ -0,0 +1,30 @@
+import { mapGetters } from 'vuex'
+export default {
+  computed: {
+    ...mapGetters({
+      currentUserSettings: 'feathers-vuex-usersettings/current',
+      blacklistPending: 'feathers-vuex-usersettings/isPending'
+    }),
+    isBlacklisted () {
+      let { blacklist } = this.currentUserSettings || {}
+      return blacklist && blacklist.includes(this.author()._id)
+    }
+  },
+  methods: {
+    async toggleBlacklist () {
+      let message
+      try {
+        await this.$store.dispatch('feathers-vuex-usersettings/toggleBlacklist', this.author())
+        const translationKey = `component.blacklist.${this.isBlacklisted ? 'blockSuccess' : 'unblockSuccess'}`
+        message = this.$t(translationKey, {
+          name: this.author().name || this.$t('component.contribution.creatorUnknown')
+        })
+      } catch (error) {
+        message = this.$t('component.error.general')
+        throw (error)
+      } finally {
+        this.$snackbar.open({ message })
+      }
+    }
+  }
+}
