b9b637283d2e2a917042ce9eb585ac97d113805c
Robert Schäfer <git@roschaefer.de>
2018-09-20 13:33:28 +0200

    Collect service plugins under store/services

--- plugins/init-feathers-vuex.js
+++ plugins/init-feathers-vuex.js
@@ -1,14 +1,21 @@
 import createApiClient from '../helpers/createApiClient'
 import feathersVuex from 'feathers-vuex'
 
+const requireModule = require.context(
+  // The relative path holding the service modules
+  '../store/services',
+  // Whether to look in subfolders
+  false,
+  // Only include .js files (prevents duplicate imports)
+  /.js$/
+)
+
 export default async ({app, store, req, res}) => {
-  const feathersClient = createApiClient({req, res})
+  const feathersClient = createApiClient({app, req, res})
   const { service } = feathersVuex(feathersClient, { idField: '_id' })
 
-  const servicePath = 'usersettings'
-  const servicePlugin = service(servicePath, {
-    namespace: 'feathers-vuex-usersettings'
-  })
-
-  servicePlugin(store);
+  const servicePlugins = requireModule.keys().map(modulePath => requireModule(modulePath).default(feathersClient))
+  servicePlugins.forEach((servicePlugin) => {
+    servicePlugin(store);
+  });
 };
