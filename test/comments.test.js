508d2f3d503370ce0d026bc27672f42181b5acca
Armin Kunkel <armin@kunkel24.de>
2018-09-23 16:32:33 +0200

    eslint fixes

--- test/comments.test.js
+++ test/comments.test.js
@@ -1,35 +1,40 @@
-import test from 'ava';
-import Vuex from 'vuex';
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import Comments from '../components/Comments/Comments.vue';
-//import Icon from '../components/Global/Elements/Icon/Icon'
-import { shallowMount, createLocalVue } from '@vue/test-utils';
+import test from 'ava'
+import Vuex from 'vuex'
+import VueRouter from 'vue-router'
+import Comments from '../components/Comments/Comments.vue'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
 
-const localVue = createLocalVue();
-localVue.use(Vuex);
-localVue.use(VueRouter);
-const router = new VueRouter();
-let store;
+const localVue = createLocalVue()
+localVue.use(Vuex)
+localVue.use(VueRouter)
+const router = new VueRouter()
+let store
 
 test.beforeEach(() => {
   const getters = {
     'auth/user': () => { return {} },
     'comments/all': () => [],
     'comments/count': () => 0,
-    'comments/isLoading': () => false,
-  };
+    'comments/isLoading': () => false
+  }
   const actions = {
     'comments/fetchByContributionId': () => [],
     'comments/subscribe': () => {}
-  };
+  }
   store = new Vuex.Store({
     getters,
     actions
-  });
-});
+  })
+})
 
 test('lol', t => {
-  const wrapper = shallowMount(Comments, { store, localVue, propsData: { post: {}}, comments: [], router});
-  t.true(wrapper.is('i'));
-});
+  const wrapper = shallowMount(Comments, {
+    store,
+    localVue,
+    propsData: {
+      post: {}
+    },
+    comments: [],
+    router})
+  t.true(wrapper.is('div'))
+})
